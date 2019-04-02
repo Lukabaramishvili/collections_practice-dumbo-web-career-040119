ced4471b177b6580ebac1c11835cae7b0a1d0fbb
Luka Baramishvili <lukabaramishvili@gmail.com>
2019-04-02 02:14:00 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,37 @@
+def sort_array_asc(array)
+  array.sort
+end
+
+def sort_array_desc(array)
+  array.sort.reverse
+end 
+
+def sort_array_char_count(array)
+  array.sort_by {|word| word.length}
+end 
+
+def swap_elements(array)
+  array[1], array[2] = array[2], array[1]
+  return array
+end 
+
+def reverse_array(num)
+  num.reverse 
+end 
+
+def kesha_maker(array)
+  words = array.split(" ")
+  words.each do |word|
+    
+end 
+
+
+
+
+
+
+
+
+
+
 
